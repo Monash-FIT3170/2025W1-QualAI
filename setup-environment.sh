7ceec33b2145ceee8531a3ff40917b8da3e89e1c
Kays Beslen <kbes0005@student.monash.edu>
2025-05-04 19:21:19 +1000

    Added a TypeScript client to interface with MongoDB.
    - This client consists of wrappers for a MongoDB collection, a MongoDB database, and a MongoClient
    - Currently supports functionality of inserting, removing, finding and renaming documents

---
+++ setup-environment.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# Packages.
+npm install mongodb
+npm install dotenv
+
+# Types.
+npm install --save-dev @types/mongodb
+npm install --save-dev @types/node
---
+++ src/mongodb/ts-client/DocumentStore.ts
@@ -0,0 +1,43 @@
+
+import { MongoClient } from "mongodb";
+import { Database } from "./Database";
+
+// TODO: Move this to the appropriate location.
+import * as dotenv from "dotenv";
+dotenv.config();
+
+/**
+ * A document store is a class wrapping a {@link MongoClient} instance.
+ *
+ * Each document store consists of several databases, each database consists of several collections,
+ * and each collection consists of several documents.
+ *
+ * @author Kays Beslen
+ */
+export class DocumentStore {
+    /** The URI to access the MongoDB client. */
+    private static readonly URI: string = process.env.MONGO_URI;
+
+    /** The MongoDB client being wrapped. */
+    private readonly _client: MongoClient;
+
+    public constructor() {
+        this._client = new MongoClient(DocumentStore.URI);
+    }
+
+    /**
+     * Creates a new {@link Database} with the specified name.
+     *
+     * @param databaseName the name for the new database
+     *
+     * @return the created database
+     */
+    public createDatabase(databaseName: string) : Database {
+        return Database.fromClient(this._client.db(databaseName));
+    }
+
+    /** The MongoDB client instance being wrapped by this class. */
+    public client(): MongoClient {
+        return this._client;
+    }
+}
