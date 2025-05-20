8a1a69163763b4cf1a80ee8440f899f1ad0a7e3f
lbos0001 <lbos0001@student.monash.edu>
2025-05-21 03:23:45 +1000

    removed comments

--- src/backend/app.py
+++ src/backend/app.py
@@ -18,8 +18,8 @@ def initialise_vector_database() -> tuple[Neo4JInteractor, TextVectoriser]:
 
 def register_upload_routes(app: Flask) -> None:
     collection = initialise_collection()
-    #vector_db, vectoriser = initialise_vector_database()
-    document_uploader = DocumentUploader(collection)#, vector_db, vectoriser)
+    vector_db, vectoriser = initialise_vector_database()
+    document_uploader = DocumentUploader(collection, vector_db, vectoriser)
     document_uploader.register_routes(app)
 
 def start_app() -> None:
--- src/backend/upload/DocumentUploader.py
+++ src/backend/upload/DocumentUploader.py
@@ -11,11 +11,11 @@ from mongodb.DocumentStore import DocumentStore
 class DocumentUploader:
   
     def __init__(
-        self, collection: DocumentStore.Collection):#, vector_database: Neo4JInteractor, vectoriser: TextVectoriser
-  #  ) -> None:
+        self, collection: DocumentStore.Collection, vector_database: Neo4JInteractor, vectoriser: TextVectoriser
+    ) -> None:
         self.__collection = collection
-       # self.__vector_database = vector_database
-      #  self.__vectoriser = vectoriser
+        self.__vector_database = vector_database
+        self.__vectoriser = vectoriser
 
     def register_routes(self, app: Flask) -> None:
         @app.route('/upload', methods=['POST'])
@@ -57,4 +57,4 @@ class DocumentUploader:
         audio_transcriber = AudioTranscriber()
         transcribed_text = audio_transcriber.transcribe(path)
         self.__collection.add_document(name, transcribed_text)
-        #self.__vector_database.store_multiple_vectors(self.__vectoriser.chunk_and_embed_text(transcribed_text), name)
+        self.__vector_database.store_multiple_vectors(self.__vectoriser.chunk_and_embed_text(transcribed_text), name)
