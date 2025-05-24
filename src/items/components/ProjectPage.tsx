a81b899126e161ccac57c4c15dfc819aa076eaa4
dgun0024 <dgun0024@student.monash.edu>
2025-05-24 16:32:04 +1000

    Put all files into one folder.

---
+++ src/items/components/ProjectPage.tsx
@@ -0,0 +1,18 @@
+
+import Sidebar from './Sidebar';
+import Chatbot from './Chatbot';
+import RichTextEditor from '@/items/editor/RichTextEditor';
+
+const ProjectPage = () => {
+  return (
+    <div className="min-h-screen flex">
+      <Sidebar />
+      <main className="flex-1 p-6">
+        <RichTextEditor />
+      </main>
+      <Chatbot />
+    </div>
+  );
+};
+
+export default ProjectPage;
