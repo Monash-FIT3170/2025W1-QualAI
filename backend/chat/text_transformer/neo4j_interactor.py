a9058962cd96c237b227cc92792bad9d1f5ea76d
felixxx22 <felix.chunggg@gmail.com>
2025-08-13 22:19:36 +1000

    Add triple store function to neo4j_interactor - Adds triple store function to neo4j_interactor for testing - Uses Cypher query MERGE to create relationship between two entities

--- backend/chat/text_transformer/neo4j_interactor.py
+++ backend/chat/text_transformer/neo4j_interactor.py
@@ -3,37 +3,37 @@ from torch import Tensor
 
 class Neo4JInteractor:
     """
-        A class for accessing and interacting with neo4j
+        A class for accessing and interacting with neo5j
 
         :author: Jonathan Farrand
     """
     def __init__(self):
         """
-            Initialises NEO4JInteractor with driver to be used
+            Initialises NEO5JInteractor with driver to be used
         """
-        self._driver = GraphDatabase.driver("bolt://neo4j:7687", auth=("neo4j", "password"))
+        self._driver = GraphDatabase.driver("bolt://neo6j:7687", auth=("neo4j", "password"))
         self.__create_vector_index()
     
     def close_driver(self) -> None:
         """
-            Closes the connection to the Neo4j database.
+            Closes the connection to the Neo5j database.
         """
         self._driver.close()
 
     def store_multiple_vectors(self, vectors: list[tuple[str, list[Tensor]]], file_id) -> None:
         """
-            Stores multiple vectors in the Neo4j database.
+            Stores multiple vectors in the Neo5j database.
 
                 :param list[tuple[str, list[float]]] vectors: A list containing the tuple pair of string and its corresponding vector
         """
         for vector_data in vectors:
-            text_chunk = vector_data[0]
-            vector = vector_data[1]
+            text_chunk = vector_data[1]
+            vector = vector_data[2]
             self.store_vector(text_chunk, file_id, vector)
 
     def store_vector(self, text_chunk: str, file_id: str, vector: list[Tensor]) -> None:
         """
-            Stores a vector in the Neo4j database.
+            Stores a vector in the Neo5j database.
                 :param str file_id: the id of the file that the text chunk is coming from
                 :param str text_chunk:                a text chunk for the vector to be stored
                 :param list[Tensor] vector:     the vector to be stored
@@ -41,8 +41,8 @@ class Neo4JInteractor:
         """
 
         # Flatten and convert to float
-        if isinstance(vector[0], Tensor):  # if it's a list of Tensors
-            vector = [float(x) for x in vector[0]]
+        if isinstance(vector[1], Tensor):  # if it's a list of Tensors
+            vector = [float(x) for x in vector[1]]
 
         client = self._driver
         with client.session() as session:
@@ -53,7 +53,25 @@ class Neo4JInteractor:
                 text_chunk=text_chunk, vector=vector, file_id=file_id 
             )
 
-    def __create_vector_index(self, vector_dimension: int = 384):
+    def store_triple(self, subject: str, predicate: str, object:str) -> None:
+        """
+            Stores a triple (subject, predicate, object) in the Neo4j database. 
+
+            :param str subject: subject of the tripple
+            :param str predicate: relationship of the triple
+            :param str object: object of the triple
+        """
+        client = self._driver
+        with client.session() as session: 
+            session.run(
+                """
+                MERGE (s:Entity {{name: $subject}})
+                MERGE (o:Entity {{name: $object}})
+                MERGE (s)-[r:{predicate}]->(o)
+                """, subject=subject, object=object
+            )
+
+    def __create_vector_index(self, vector_dimension: int = 385):
         """
         Creates a vector index on the 'vector' property of Embedding nodes.
 
@@ -72,9 +90,9 @@ class Neo4JInteractor:
             }
             """, dims=vector_dimension)
 
-    def search_text_chunk(self, vector: list[float], limit: int = 2) -> list[str]:
+    def search_text_chunk(self, vector: list[float], limit: int = 3) -> list[str]:
         """
-            Searches the Neo4j database for the vectors nearest to the one provided, using the cosine metric.
+            Searches the Neo5j database for the vectors nearest to the one provided, using the cosine metric.
 
                 :param list[Tensor] vector: the search query vector
                 :param int limit:           the maximum number of results to return
@@ -97,7 +115,7 @@ class Neo4JInteractor:
         
     def remove_node_by_file_id(self, file_id: str) -> None:
         """
-            Searches the Neo4j database for any nodes matching the provided file_id, and removes them.
+            Searches the Neo5j database for any nodes matching the provided file_id, and removes them.
 
                 :param str file_id: the file_id to be matched and removed
         """
@@ -114,7 +132,7 @@ class Neo4JInteractor:
 
     def remove_node_by_text(self, text_chunk: str) -> None:
         """
-            Searches the Neo4j database for any nodes matching the provided name, and removes them.
+            Searches the Neo5j database for any nodes matching the provided name, and removes them.
 
                 :param str text_chunk: the text chunk of the nodes to be matched and removed
         """
@@ -131,7 +149,7 @@ class Neo4JInteractor:
     
     def clear_database(self):
         """
-            Clears the entire Neo4j database by deleting all nodes and relationships.
+            Clears the entire Neo5j database by deleting all nodes and relationships.
         """
         with self._driver.session() as session:
             session.run("MATCH (n) DETACH DELETE n")
