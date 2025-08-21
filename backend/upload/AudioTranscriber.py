445db1ad2eb14d62ab661515f28b3226818365f8
Kade Lucy <kade.lucy@y7mail.com>
2025-08-21 22:25:51 +1000

    Modify AudioTranscriber to use new diarization method

--- backend/upload/AudioTranscriber.py
+++ backend/upload/AudioTranscriber.py
@@ -1,5 +1,6 @@
-import whisper
-
+import subprocess
+import os
+from upload.video_to_audio import convert_media
 
 # whisper-diarization
 class AudioTranscriber:
@@ -16,7 +17,8 @@ class AudioTranscriber:
         Initialises the transcriber by loading in the 
         OpenAI whisper model to base configuration and
         """
-        self.model = whisper.load_model("base")
+        # Path to whisper-diarization model
+        self.diarize_path = "./upload/whisper-diarization/diarize.py"
 
     def transcribe(self, audio_filepath: str):
         """
@@ -26,12 +28,36 @@ class AudioTranscriber:
 
             :return str: the transcribed text
         """
-        audio = whisper.load_audio(audio_filepath)
-        result = whisper.transcribe(model=self.model, audio=audio)
+
+        # Ensure file is converted to mp3
+        filepath_mp3 = convert_media(audio_filepath)
+
+        # Run diarize process as subprocess
+        subprocess.run(
+            ['/opt/venv/bin/python', 
+             self.diarize_path, 
+             '-a', audio_filepath,
+             "--batch-size", '1']) # Reduces load on docker to avoid crashing 
         
-        # audio = whisper.pad_or_trim(audio)
-        # result = whisper.transcribe(model=self.model, audio=audio)
-        return result["text"]
+        # Retrieve text file transcription
+        filepath_txt = filepath_mp3.with_suffix(".txt")
+        filepath_srt = filepath_mp3.with_suffic(".srt")
+        transcript = open(filepath_txt)
+
+        # TODO: Delete created transcripts (.txt and .srt file)
+        if os.path.exists(filepath_txt):
+            try:
+                os.remove(filepath_txt)
+            except OSError as e:
+                print(f'Error: {filepath_txt}: {e.strerror}')
+        
+        if os.path.exists(filepath_srt):
+            try:
+                os.remove(filepath_srt)
+            except OSError as e:
+                print(f'Error: {filepath_srt}: {e.strerror}')
+
+        return transcript.read()
  
 if __name__ == "__main__":
     transcriber = AudioTranscriber()
