dae15b4672249b24cfb602822f7c17fe0bdb765e
felixxx22 <felix.chunggg@gmail.com>
2025-09-02 20:45:19 +1000

    Fix neo4j upload statement - Fixes bug where tensors were incorrectly references - Allows neo4j_interactor to correctly upload vectors

--- backend/chat/text_transformer/neo4j_interactor.py
+++ backend/chat/text_transformer/neo4j_interactor.py
@@ -4,13 +4,13 @@ import re
 
 class Neo4JInteractor:
     """
-        A class for accessing and interacting with neo5j
+        A class for accessing and interacting with NEO4J
 
         :author: Jonathan Farrand
     """
     def __init__(self):
         """
-            Initialises NEO5JInteractor with driver to be used
+            Initialises NEO4JInteractor with driver to be used
         """
         # self._driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
         # using one below for testing, top one isn't working for me - Rohan
@@ -20,7 +20,7 @@ class Neo4JInteractor:
     
     def close_driver(self) -> None:
         """
-            Closes the connection to the Neo5j database.
+            Closes the connection to the NEO4J database.
         """
         self._driver.close()
 
@@ -38,18 +38,18 @@ class Neo4JInteractor:
     
     def store_multiple_vectors(self, vectors: list[tuple[str, list[Tensor]]], file_id) -> None:
         """
-            Stores multiple vectors in the Neo5j database.
+            Stores multiple vectors in the NEO4J database.
 
                 :param list[tuple[str, list[float]]] vectors: A list containing the tuple pair of string and its corresponding vector
         """
         for vector_data in vectors:
-            text_chunk = vector_data[1]
-            vector = vector_data[2]
+            text_chunk = vector_data[0]
+            vector = vector_data[1]
             self.store_vector(text_chunk, file_id, vector)
 
     def store_vector(self, text_chunk: str, file_id: str, vector: list[Tensor]) -> None:
         """
-            Stores a vector in the Neo5j database.
+            Stores a vector in the NEO4J database.
                 :param str file_id: the id of the file that the text chunk is coming from
                 :param str text_chunk:                a text chunk for the vector to be stored
                 :param list[Tensor] vector:     the vector to be stored
@@ -57,8 +57,8 @@ class Neo4JInteractor:
         """
 
         # Flatten and convert to float
-        if isinstance(vector[1], Tensor):  # if it's a list of Tensors
-            vector = [float(x) for x in vector[1]]
+        if isinstance(vector[0], Tensor):  # if it's a list of Tensors
+            vector = [float(x) for x in vector[0]]
 
         client = self._driver
         with client.session() as session:
@@ -135,7 +135,7 @@ class Neo4JInteractor:
 
     def search_text_chunk(self, vector: list[float], limit: int = 3) -> list[str]:
         """
-            Searches the Neo5j database for the vectors nearest to the one provided, using the cosine metric.
+            Searches the NEO4J database for the vectors nearest to the one provided, using the cosine metric.
 
                 :param list[Tensor] vector: the search query vector
                 :param int limit:           the maximum number of results to return
@@ -158,7 +158,7 @@ class Neo4JInteractor:
         
     def remove_node_by_file_id(self, file_id: str) -> None:
         """
-            Searches the Neo5j database for any nodes matching the provided file_id, and removes them.
+            Searches the NEO4J database for any nodes matching the provided file_id, and removes them.
 
                 :param str file_id: the file_id to be matched and removed
         """
@@ -175,7 +175,7 @@ class Neo4JInteractor:
 
     def remove_node_by_text(self, text_chunk: str) -> None:
         """
-            Searches the Neo5j database for any nodes matching the provided name, and removes them.
+            Searches the NEO4J database for any nodes matching the provided name, and removes them.
 
                 :param str text_chunk: the text chunk of the nodes to be matched and removed
         """
@@ -192,7 +192,7 @@ class Neo4JInteractor:
     
     def clear_database(self):
         """
-            Clears the entire Neo5j database by deleting all nodes and relationships.
+            Clears the entire NEO4J database by deleting all nodes and relationships.
         """
         with self._driver.session() as session:
             session.run("MATCH (n) DETACH DELETE n")
