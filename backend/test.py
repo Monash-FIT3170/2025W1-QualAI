68aa20a4cfd0a6051d1044d4da457ef0c4918ce4
MeV2V2 <jaeminp707@gmail.com>
2025-09-05 08:23:50 +1000

    triple extraction attempt

--- backend/test.py
+++ backend/test.py
@@ -14,7 +14,7 @@ model_name = "Babelscape/rebel-large"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-def define_sentences(file_path):
+def define_sentences(file_path): 
     try:
         with open(path, 'r', encoding="utf-8") as file:
             text = file.read()
@@ -22,24 +22,52 @@ def define_sentences(file_path):
         sentences = sent_tokenize(text)
         return(sentences)
 
-
     except FileNotFoundError:
         print(f"{path} not found")
 
-def extract_triples(sentence):
-    inputs = tokenizer(sentence, return_tensors="pt")
-    outputs = model.generate(**inputs, max_length=256)
-    decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return decoded
+
+def extract_triplets(text):
+    triplets = []
+    relation, subject, relation, object_ = '', '', '', ''
+    text = text.strip()
+    current = 'x'
+    for token in text.replace("<s>", "").replace("<pad>", "").replace("</s>", "").split():
+        if token == "<triplet>":
+            current = 't'
+            if relation != '':
+                triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+                relation = ''
+            subject = ''
+        elif token == "<subj>":
+            current = 's'
+            if relation != '':
+                triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+            object_ = ''
+        elif token == "<obj>":
+            current = 'o'
+            relation = ''
+        else:
+            if current == 't':
+                subject += ' ' + token
+            elif current == 's':
+                object_ += ' ' + token
+            elif current == 'o':
+                relation += ' ' + token
+    if subject != '' and relation != '' and object_ != '':
+        triplets.append({'head': subject.strip(), 'type': relation.strip(),'tail': object_.strip()})
+    return triplets
+
 
 if __name__ == '__main__':
     sentences = define_sentences(path)
 
-    triples = []
-    for sentence in sentences:
-        result = extract_triples(sentence)
-        triples.append(result)
-        print(result)
+    # print(sentences)
+
+    # triples = []
+    # for sentence in sentences:
+    #     result = extract_triples(sentence)
+    #     triples.append(result)
+    #     print(result)
 
     
 
