3e44179998433105f7922a3ab822b78a47515c09
jae <96039674+MeV2V2@users.noreply.github.com>
2025-09-30 15:18:05 +1000

    fixed docstring errors and added typehinting

--- backend/chat/llm_client/llm_client.py
+++ backend/chat/llm_client/llm_client.py
@@ -3,16 +3,16 @@ from abc import ABC, abstractmethod
 class LLMClient(ABC):
     """
     At interface for LLM interation. Connects to LLM and provides methods for connection. 
-    Different strategies (deepseek, gemini) implement this
+    Different strategies (deepseek, gemini) implement this.
     
     :author: Felix Chung
+    :modified by: Jaemin Park
     """
     @abstractmethod
     def init_client(self): 
         """
-        Initilises the chatbot class with API url and key
+        Initilises the chatbot class with API url and key.
         """
-        pass
 
     @abstractmethod
     def extract_triples(self, text: str) -> list[tuple[str,str,str]]:
@@ -20,19 +20,19 @@ class LLMClient(ABC):
         Extracts knowledge triples from the input text in the format:
         (Subject, Predicate, Object)
 
-        :param text: The input text
-        :return: A list of extracted triples
+        :param text: The input text.
+        :return: A list of extracted triples.
         """
-        pass 
 
     @abstractmethod
-    def chat_with_model_context_injection(self, context_text, message: str):
+    def chat_with_model_context_injection(self, context_text, message: str) -> str:
         """
-        Sends a message to the google gemini API with additional context injected as a system message
+        Sends a message to the LLM model with additional context injected as a system message.
 
         :param context_text: The external context (e.g., from a document).
         :param message: The user's question.
-        :return: The JSON response from the API.
+
+        :return: A string which is the response from the LLM to the user question.
         """
 
     @abstractmethod
@@ -40,17 +40,18 @@ class LLMClient(ABC):
         """
         Chat with an LLM model with additional knowledge triples injected as context.
 
-        :param triple: A list of knowledge triples in the form (SUBJECT, OBJECT, PREDICATE)
-        :param message: The user's question
+        :param triple: A list of knowledge triples in the form (SUBJECT, OBJECT, PREDICATE).
+        :param message: The user's question.
         
-        :return: A string which is the response from the LLM to the user question
+        :return: A string which is the response from the LLM to the user question.
         """
 
     @abstractmethod
-    def chat_with_model(self, message: str):
+    def chat_with_model(self, message: str) -> str:
         """
         Sends a basic message to the model and returns the response.
 
-        :param message: The message to send to the model.
-        :return: The JSON response from the API.
+        :param message: The user message to send to the model.
+
+        :return: A string which is the response from the LLM to the user question.
         """
